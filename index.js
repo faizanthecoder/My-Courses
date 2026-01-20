148fd2888b75507b2240b53653ddf973621a6205
faizanthecoder <faizanthecoder@gmail.com>
2026-01-20 19:56:44 +0500

    .

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-l  
+let course=document.getElementById("courses")
+let dropdown=document.querySelector(".drop-down")
+
+course.addEventListener("mouseenter",function(){
+    dropdown.classList.add("toggle")
+})
