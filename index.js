f6643773751ccc28dad05c61f40bb5ff92a81a64
faizanthecoder <faizanthecoder@gmail.com>
2026-01-21 20:02:54 +0500

    .

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-let course=document.getElementById("courses")
-let dropdown=document.querySelector(".drop-down")
+// let course=document.getElementById("courses")
+// let dropdown=document.querySelector(".drop-down")
 
-course.addEventListener("mouseenter",function(){
-    dropdown.classList.add("toggle")
-})
+// course.addEventListener("onmo",function(){
+//     dropdown.classList.add("toggle")
+// })
+// course.addEventListener("mouseenter",function(){
+//     dropdown.classList.add("toggle")
+// })
+
+function showDropdown() {
+  document.getElementById("myDropdown").style.display = "block";
+}
+
+function hideDropdown() {
+  document.getElementById("myDropdown").style.display = "none";
+}
